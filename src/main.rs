1c77bcce75b86d9771d43ffdd823eb98c3a545bc
Theodor Peifer <teddypeifer@gmail.com>
2025-09-15 21:49:59 +0200

    add string type and tests

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
 use core::panic;
 
-#[derive(Debug)]
-pub enum TokenType {
-    Let,
-    Ident(String),
+#[derive(Debug, PartialEq, Clone)]
+pub enum Primitive {
+    Int,
+    Float,
+    Str,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum Literal {
+    Int(String),
+    Float(String),
+    String(String),
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum TokenKind {
+    Declare(Primitive),
+    Identifier(String),
+    Literal(Literal),
     Assign,
-    Number(String),
     Add,
     Sub,
     Mult,
@@ -19,30 +33,8 @@ pub enum TokenType {
 
 #[derive(Debug)]
 pub struct Token {
-    token_type: TokenType,
+    kind: TokenKind,
     pos: usize,
-}
-
-pub enum OpKind {
-    Add,
-    Sub,
-    Mult,
-    Div,
-}
-
-pub enum Expr {
-    Number(i64),
-    Ident(String),
-    BinOp {
-        op: OpKind,
-        left: Box<Expr>,
-        right: Box<Expr>,
-    },
-}
-
-pub enum Statement {
-    Let { name: String, expr: Expr },
-    Print { expr: Expr },
 }
 
 struct Lexer {
@@ -72,16 +64,24 @@ impl Lexer {
 
         let token: String = self.chars[start..self.cur_pos].iter().collect();
         match token.as_str() {
-            "let" => self.tokens.push(Token {
-                token_type: TokenType::Let,
+            "int" => self.tokens.push(Token {
+                kind: TokenKind::Declare(Primitive::Int),
+                pos: start,
+            }),
+            "float" => self.tokens.push(Token {
+                kind: TokenKind::Declare(Primitive::Float),
+                pos: start,
+            }),
+            "str" => self.tokens.push(Token {
+                kind: TokenKind::Declare(Primitive::Str),
                 pos: start,
             }),
             "print" => self.tokens.push(Token {
-                token_type: TokenType::Print,
+                kind: TokenKind::Print,
                 pos: start,
             }),
             _ => self.tokens.push(Token {
-                token_type: TokenType::Ident(token),
+                kind: TokenKind::Identifier(token),
                 pos: start,
             }),
         }
@@ -97,12 +97,40 @@ impl Lexer {
             break;
         }
 
-        let token: String = self.chars[start..self.cur_pos].iter().collect();
-        println!("{}", token);
+        let numeric_token: String = self.chars[start..self.cur_pos].iter().collect();
+
         self.tokens.push(Token {
-            token_type: TokenType::Number(token),
+            kind: TokenKind::Literal(if numeric_token.contains('.') {
+                Literal::Float(numeric_token)
+            } else {
+                Literal::Int(numeric_token)
+            }),
             pos: start,
         });
+    }
+
+    fn handle_string(&mut self) {
+        self.cur_pos += 1;
+
+        let start = self.cur_pos;
+        while let Some(&ch) = self.chars.get(self.cur_pos) {
+            if ch != '"' {
+                self.cur_pos += 1;
+                continue;
+            }
+            break;
+        }
+
+        let string_token: String = self.chars[start..self.cur_pos].iter().collect();
+
+        self.tokens.push(Token {
+            kind: TokenKind::Literal(Literal::String(string_token)),
+            pos: start,
+        });
+
+        self.cur_pos += 1;
+
+        println!("{:?}", self.tokens);
     }
 
     fn tokenize(&mut self) -> &Vec<Token> {
@@ -119,48 +147,45 @@ impl Lexer {
                     self.handle_numeric();
                     continue;
                 }
+                '"' => {
+                    self.handle_string();
+                    continue;
+                }
                 '=' => self.tokens.push(Token {
-                    token_type: TokenType::Assign,
+                    kind: TokenKind::Assign,
                     pos: self.cur_pos,
                 }),
-
                 '+' => self.tokens.push(Token {
-                    token_type: TokenType::Add,
+                    kind: TokenKind::Add,
                     pos: self.cur_pos,
                 }),
-
                 '-' => self.tokens.push(Token {
-                    token_type: TokenType::Sub,
+                    kind: TokenKind::Sub,
                     pos: self.cur_pos,
                 }),
-
                 '*' => self.tokens.push(Token {
-                    token_type: TokenType::Mult,
+                    kind: TokenKind::Mult,
                     pos: self.cur_pos,
                 }),
-
                 '/' => self.tokens.push(Token {
-                    token_type: TokenType::Div,
+                    kind: TokenKind::Div,
                     pos: self.cur_pos,
                 }),
-
                 '(' => self.tokens.push(Token {
-                    token_type: TokenType::LParen,
+                    kind: TokenKind::LParen,
                     pos: self.cur_pos,
                 }),
-
                 ')' => self.tokens.push(Token {
-                    token_type: TokenType::RParen,
+                    kind: TokenKind::RParen,
                     pos: self.cur_pos,
                 }),
-
                 ';' => self.tokens.push(Token {
-                    token_type: TokenType::EOS,
+                    kind: TokenKind::EOS,
                     pos: self.cur_pos,
                 }),
                 '\0' => {
                     self.tokens.push(Token {
-                        token_type: TokenType::EOF,
+                        kind: TokenKind::EOF,
                         pos: self.cur_pos,
                     });
                     break;
@@ -181,9 +206,195 @@ impl Lexer {
 }
 
 fn main() {
-    let code = "let a = 3 + 2 * 4;\0";
+    //let code = "let a = ((3 + 2.5) * 4) / .05;\nlet b = a * 2;print(b);\0";
+    let code = "print(\"hello\");\0";
+
     let mut lexer = Lexer::new(code);
     let tokens = lexer.tokenize();
 
-    println!("{:?}", tokens);
+    println!("{:#?}", tokens);
+}
+
+/*
+pub enum OpKind {
+    Add,
+    Sub,
+    Mult,
+    Div,
+}
+
+pub enum Expr {
+    Number(i64),
+    Ident(String),
+    BinOp {
+        op: OpKind,
+        left: Box<Expr>,
+        right: Box<Expr>,
+    },
+}
+
+pub enum Statement {
+    Let { name: String, expr: Expr },
+    Print { expr: Expr },
+}
+*/
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn lex(input: &str) -> Vec<TokenKind> {
+        let mut lexer = Lexer::new(&(input.to_owned() + "\0"));
+        lexer.tokenize().iter().map(|t| t.kind.clone()).collect()
+    }
+
+    #[test]
+    fn test_int_declaration() {
+        let tokens = lex("int a = 42;");
+        assert_eq!(
+            tokens,
+            vec![
+                TokenKind::Declare(Primitive::Int),
+                TokenKind::Identifier("a".into()),
+                TokenKind::Assign,
+                TokenKind::Literal(Literal::Int("42".into())),
+                TokenKind::EOS,
+                TokenKind::EOF,
+            ]
+        );
+    }
+
+    #[test]
+    fn test_float_declaration() {
+        let tokens = lex("float pi = 3.14;");
+        assert_eq!(
+            tokens,
+            vec![
+                TokenKind::Declare(Primitive::Float),
+                TokenKind::Identifier("pi".into()),
+                TokenKind::Assign,
+                TokenKind::Literal(Literal::Float("3.14".into())),
+                TokenKind::EOS,
+                TokenKind::EOF,
+            ]
+        );
+    }
+
+    #[test]
+    fn test_print_statement() {
+        let tokens = lex("print(x);");
+        assert_eq!(
+            tokens,
+            vec![
+                TokenKind::Print,
+                TokenKind::LParen,
+                TokenKind::Identifier("x".into()),
+                TokenKind::RParen,
+                TokenKind::EOS,
+                TokenKind::EOF,
+            ]
+        );
+    }
+
+    #[test]
+    fn test_simple_expression() {
+        let tokens = lex("int a = 3 + 5;");
+        assert_eq!(
+            tokens,
+            vec![
+                TokenKind::Declare(Primitive::Int),
+                TokenKind::Identifier("a".into()),
+                TokenKind::Assign,
+                TokenKind::Literal(Literal::Int("3".into())),
+                TokenKind::Add,
+                TokenKind::Literal(Literal::Int("5".into())),
+                TokenKind::EOS,
+                TokenKind::EOF,
+            ]
+        );
+    }
+
+    #[test]
+    fn test_parentheses_and_mult_div() {
+        let tokens = lex("(2 * 4) / .5;");
+        assert_eq!(
+            tokens,
+            vec![
+                TokenKind::LParen,
+                TokenKind::Literal(Literal::Int("2".into())),
+                TokenKind::Mult,
+                TokenKind::Literal(Literal::Int("4".into())),
+                TokenKind::RParen,
+                TokenKind::Div,
+                TokenKind::Literal(Literal::Float(".5".into())),
+                TokenKind::EOS,
+                TokenKind::EOF,
+            ]
+        );
+    }
+
+    #[test]
+    fn test_identifier_with_underscore() {
+        let tokens = lex("int my_var = 1;");
+        assert_eq!(
+            tokens,
+            vec![
+                TokenKind::Declare(Primitive::Int),
+                TokenKind::Identifier("my_var".into()),
+                TokenKind::Assign,
+                TokenKind::Literal(Literal::Int("1".into())),
+                TokenKind::EOS,
+                TokenKind::EOF,
+            ]
+        );
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_invalid_character() {
+        let _ = lex("int a = 5 $ 2;");
+    }
+
+    #[test]
+    fn test_simple_string() {
+        let tokens = lex("str a = \"hello\";");
+        assert_eq!(
+            tokens,
+            vec![
+                TokenKind::Declare(Primitive::Str),
+                TokenKind::Identifier("a".into()),
+                TokenKind::Assign,
+                TokenKind::Literal(Literal::String("hello".to_string())),
+                TokenKind::EOS,
+                TokenKind::EOF,
+            ]
+        );
+    }
+
+    #[test]
+    fn test_string_with_expression() {
+        let tokens = lex("\"4 + 3\"");
+        assert_eq!(
+            tokens,
+            vec![
+                TokenKind::Literal(Literal::String("4 + 3".to_string())),
+                TokenKind::EOF
+            ]
+        );
+    }
+
+    #[test]
+    fn test_empty_string() {
+        let tokens = lex("print(\"\")");
+        assert_eq!(
+            tokens,
+            vec![
+                TokenKind::Print,
+                TokenKind::LParen,
+                TokenKind::Literal(Literal::String("".to_string())),
+                TokenKind::RParen,
+                TokenKind::EOF
+            ]
+        );
+    }
 }
